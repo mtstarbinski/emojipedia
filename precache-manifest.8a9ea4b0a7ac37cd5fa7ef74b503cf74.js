444d1aa793fcc0df0e64439ac9bb912ad760a558
Mark <mtstarbinski@outlook.com>
2022-01-03 19:18:18 -0500

    Updates

---
+++ precache-manifest.8a9ea4b0a7ac37cd5fa7ef74b503cf74.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d81aa1bb84131f7293a6d083721a7394",
+    "url": "/emojipedia/index.html"
+  },
+  {
+    "revision": "01155d618f35e37e74d4",
+    "url": "/emojipedia/static/js/2.fa00b217.chunk.js"
+  },
+  {
+    "revision": "c277a1d09bfb0fbcd967",
+    "url": "/emojipedia/static/js/main.0a3b8bed.chunk.js"
+  },
+  {
+    "revision": "b23b46efe5a8f40d9a29",
+    "url": "/emojipedia/static/js/runtime-main.76d70354.js"
+  }
+]);
